b2faf4cdaff04cee18a05c1aaddb360df7d0f5ea
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-06-06 21:55:13 +0100

    Refactored all functions to blueprint

--- quickstart2.py
+++ quickstart2.py
@@ -91,48 +91,13 @@ def doActionBlueprint(filename, actionfn, args):
         random.shuffle(filedata)
         filedata = filedata[:random.randrange(1,5)]
         for data_entry in filedata:
-            nactions = random.randrange(1,10)
             actionfn([data_entry], **args)
-            if (nactions == 0):
-                sleeptime = random.randrange(min_time_of_likeorfollow, max_time_of_likeorfollow)
-                print('Sleeping for' + str(sleeptime))
-                time.sleep(random.randrange(sleeptime))
 
 def likeByLocations():
     doActionBlueprint('Locations2.csv', session.like_by_locations,{'amount':random.randrange(1,10), 'skip_top_posts': True})
-
-
     
-# Function Like by Tags
-
-
 def likeByTags():
-    myusers = readMyFile('Tags2.csv')
-    sizemyusers = len(myusers)
-    i = 0
-    for i in range(random.randrange(1,5)):
-        temp[0] = myusers[random.randrange(0, sizemyusers)]
-        print(temp[0])
-        a = 0
-        while(a <= random.randrange(1, 10)):
-            tm = random.randrange(1, 4)
-            session.like_by_tags(temp, amount=tm, interact=False)
-            if(tm == 1):
-                a += 1
-                print("1 Like")
-            elif(tm == 2):
-                a += 2
-                print("2 Likes")
-            elif(tm == 3):
-                a += 3
-            else:
-                a += 0
-                print("sleeping a bit")
-                time.sleep(random.randrange(
-                    min_time_of_likeorfollow, max_time_of_likeorfollow))
-
-# unfollow function
-
+    doActionBlueprint('Tags2.csv', session.like_by_tags, {'amount': random.randrange(1,4), 'interact' : False})
 
 def unfollow():
         # session.unfollow_users(amount=random.randrange(20,50), onlyNotFollowMe=True, sleep_delay=60)
@@ -141,146 +106,26 @@ def unfollow():
         20, 50), onlyInstapyFollowed=True, onlyInstapyMethod='FIFO', sleep_delay=60)
     session.unfollow_users(amount=random.randrange(
         20, 50), onlyInstapyFollowed=False, onlyInstapyMethod='LIFO', sleep_delay=60)
-# Function to follow that users follow
-
 
 def followByUserFollow():
-    myusers = readMyFile('Users2.csv')
-    sizemyusers = len(myusers)
-    i = 0
-    for i in range(random.randrange(1,5)):
-        temp[0] = myusers[random.randrange(0, sizemyusers)]
-        print(temp[0])
-        a = 0
-        while(a <= random.randrange(1, 10)):
-            tm = random.randrange(1, 4)
-            session.follow_user_following(
-                temp, amount=tm, randomize=True, sleep_delay=60)
-            if(tm == 1):
-                a += 1
-                print("1 follow")
-            elif(tm == 2):
-                a += 2
-                print("2 follow")
-            elif(tm == 3):
-                a += 3
-            else:
-                a += 0
-                print("sleeping a bit")
-                time.sleep(random.randrange(
-                    min_time_of_likeorfollow, max_time_of_likeorfollow))
-
-# Function to steal the followers of the users
-
-
+    doActionBlueprint('Users2.csv', session.follow_user_following, {'amount': random.randrange(1,4), 'randomize': True, 'sleep_delay': 60})
+    
 def followByUserFollowers():
-    myusers = readMyFile('Users2.csv')
-    sizemyusers = len(myusers)
-    i = 0
-    for i in range(random.randrange(1,5)):
-        temp[0] = myusers[random.randrange(0, sizemyusers)]
-        print(temp[0])
-        a = 0
-        while(a <= random.randrange(1, 10)):
-            tm = random.randrange(1, 4)
-            session.follow_user_followers(
-                temp, amount=tm, randomize=True, sleep_delay=60)
-            if(tm == 1):
-                a += 1
-                print("1 follow")
-            elif(tm == 2):
-                a += 2
-                print("2 follow")
-            elif(tm == 3):
-                a += 3
-            else:
-                a += 0
-                print("sleeping a bit")
-                time.sleep(random.randrange(
-                    min_time_of_likeorfollow, max_time_of_likeorfollow))
-
-# Function to like in the timeline
+    doActionBlueprint('Users2.csv', session.follow_user_followers, {'amount': random.randrange(1,4), 'randomize': True, 'sleep_delay': 60})
+    
 def followByUserLikers():
-    myusers = readMyFile('Users2.csv')
-    sizemyusers = len(myusers)
-    i = 0
-    for i in range(random.randrange(1,5)):
-        temp[0] = myusers[random.randrange(0, sizemyusers)]
-        print(temp[0])
-        a = 0
-        while(a <= random.randrange(1, 10)):
-            tm = random.randrange(1, 4)
-            session.follow_likers (temp, photos_grab_amount = random.randrange(1, 5), follow_likers_per_photo = tm, randomize=True, sleep_delay=600, interact=True)
-            if(tm == 1):
-                a += 1
-                print("1 follow")
-            elif(tm == 2):
-                a += 2
-                print("2 follow")
-            elif(tm == 3):
-                a += 3
-            else:
-                a += 0
-                print("sleeping a bit")
-                time.sleep(random.randrange(
-                    min_time_of_likeorfollow, max_time_of_likeorfollow))
-
+    doActionBlueprint('Users2.csv', session.follow_likers, {'photos_grab_amount': random.randrange(1,5), 'follow_likers_per_photo' : random.randrange(1,4), 'randomize': True, 'sleep_delay': 600, 'interact': True})
+    
 def likeByTimeline():
     session.like_by_feed(amount=random.randrange(
         2, 32), randomize=True, unfollow=False, interact=False)
 
-
-
 def followComenters():
-    myusers = readMyFile('Users2.csv')
-    sizemyusers = len(myusers)
-    i = 0
-    for i in range(random.randrange(1,5)):
-        temp[0] = myusers[random.randrange(0, sizemyusers)]
-        print(temp[0])
-        a = 0
-        while(a <= 5):
-            tm = random.randrange(1, 4)
-            session.follow_commenters(temp, amount=tm, daysold=random.randrange(1, 200), max_pic=random.randrange(1, 60), sleep_delay=600, interact=False)
-            if(tm == 1):
-                a += 1
-                print("1 follow")
-            elif(tm == 2):
-                a += 2
-                print("2 follow")
-            elif(random.randrange(1, 4) == 3):
-                a += 3
-            else:
-                a += 0
-                print("sleeping a bit")
-                time.sleep(random.randrange(
-                    min_time_of_likeorfollow, max_time_of_likeorfollow))
-
+    doActionBlueprint('Users2.csv', session.follow_commenters, {'amount': random.randrange(1,4), 'daysold': random.randrange(1,200), 'max_pic': random.randrange(1,60), 'sleep_delay': 600, 'interact':False})
+    
 def followSmartHashes():
-    myusers = readMyFile('smarthash.csv')
-    sizemyusers = len(myusers)
-    i = 0
-    for i in range(random.randrange(1,5)):
-        temp[0] = myusers[random.randrange(0, sizemyusers)]
-        print(temp[0])
-        a = 0
-        while(a <= 5):
-            tm = random.randrange(1, 4)
-            session.follow_commenters(temp, amount=tm, daysold=random.randrange(1, 200), max_pic=random.randrange(1, 60), sleep_delay=600, interact=False)
-            if(tm == 1):
-                a += 1
-                print("1 follow")
-            elif(tm == 2):
-                a += 2
-                print("2 follow")
-            elif(random.randrange(1, 4) == 3):
-                a += 3
-            else:
-                a += 0
-                print("sleeping a bit")
-                time.sleep(random.randrange(
-                    min_time_of_likeorfollow, max_time_of_likeorfollow))
-
+    doActionBlueprint('smarthash.csv', session.follow_commenters, {'amount': random.randrange(1,4), 'daysold': random.randrange(1,200), 'max_pic': random.randrange(1,60), 'sleep_delay': 600, 'interact':False})
+    
 
 # ---------------------------------- Run Script-------	-----------------------------
 try:
@@ -316,7 +161,7 @@ try:
                         time.sleep(random.randrange(2, 5))							
                         if(ct >= random.randrange(1, 4)):
                             session.unfollow_users(amount=random.randrange(10, 20), nonFollowers=True, style="RANDOM", unfollow_after=42*60*60,sleep_delay=random.randrange(100, 250))																				
-            likeByLocations()
+
             abc = random.randrange(1, 5)
             if abc == 1:
                 print("Welcome to func n 1")
